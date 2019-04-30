5d9d7ece57de3f284ba704a4d1bdaa57fb6e4e05
lucyweber <50153028+lucyweber@users.noreply.github.com>
2019-04-30 13:05:36 -0700

    Add files via upload

---
+++ Python section4.py
@@ -0,0 +1,8 @@
+job_applicants = ["John", "Geoff", "Brady", "Thomas"]
+heights = [61,70, 67, 64, 65]
+ints_and_strings = [1,2,3, 'four', 'five']
+empty_list = []
+
+print(job_applicants[0])
+print(job_applicants[2])
+print(job_applicants[1])
