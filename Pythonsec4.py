e8245a1095653d7aae78774e89dea404247b04aa
lucyweber <50153028+lucyweber@users.noreply.github.com>
2019-04-30 13:29:04 -0700

    Add files via upload

---
+++ Pythonsec4.py
@@ -0,0 +1,22 @@
+job_applicants = ["John", "Geoff", "Brady", "Thomas"]
+heights = [61,70, 67, 64, 65]
+ints_and_strings = [1,2,3, 'four', 'five']
+empty_list = []
+
+print(job_applicants[0])
+print(job_applicants[2])
+print(job_applicants[1])
+print(job_applicants[3])
+
+John_Skills = ['Python', 'Communication', 'Low Salary Request', 10000]
+print(John_Skills)
+Applicants = [['John', 'Python'], ['Geoff', 'Doesn’t Know Python']]
+print(Applicants)
+
+heights = [['Jenny', 61], ['Alexus', 70], ['Sam', 67], ['Grace', 64], ['Vik', 68]]
+ages = [['Aaron', 15], ['Dhruti', 16]]
+
+names = ['Jenny', 'Alexus', 'Samuel', 'Grace']
+skills = ['Python', 'R', 'NOTHING', 'Python']
+names_and_skills = zip(names, skills)
+print(names_and_skills)
